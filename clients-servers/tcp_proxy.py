b8068d4e24f4d3ad5e0c9d8cd3950ff9cbf3a84d
Duane Michael <dmichaeljr@pm.me>
2021-08-30 22:10:40 -0600

    Updated proxy script

--- clients-servers/tcp_proxy.py
+++ clients-servers/tcp_proxy.py
@@ -54,13 +54,13 @@ def response_handler(buffer):
 
 def proxy_handler(client_socket, remote_host, remote_port, receive_first):
     remote_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    remote_socket.connect((remote_host, remote_port))                           # Connect to remote host
+    remote_socket.connect((remote_host, remote_port))                                   # Connect to remote host
 
-    if receive_first:
+    if receive_first:                                                                   # Check to receive first
         remote_buffer = receive_from(remote_socket)
         hexdump(remote_buffer)
 
-    remote_buffer = response_handler(remote_buffer)
+    remote_buffer = response_handler(remote_buffer)                                     
     if len(remote_buffer):
         print("[<==] Sending %d bytes to localhost." % len(remote_buffer))
         client_socket.send(remote_buffer)
@@ -85,9 +85,56 @@ def proxy_handler(client_socket, remote_host, remote_port, receive_first):
         client_socket.send(remote_buffer)
         print("[<== Sent to localhost.")
 
-        if not len(local_buffer) or not len(remote_buffer):
+        if not len(local_buffer) or not len(remote_buffer):                             # Close the sockets when there's no data to send
             client_socket.close()
             remote_socket.close()
             print("[*] No more data. Closing connections.")
             break
 
+def server_loop(local_host, local_port, remote_host, remote_port, receive_first):
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    try:
+        server.bind((local_host, local_port))
+    except Exception as e:
+        print('Problem on bind %r' %e)
+        print("[!!] Failed to listen on %s:%d" % (local_host, local_port))
+        print("[!!] Check for other listening sockets or correct permissions.")
+        sys.exit(0)
+
+    print("[*] Listening on %s:%d" % (local_host, local_port))
+    server.listen(5)
+    while True:
+        client_socket, addr = server.accept()
+
+        # print out the local connection info
+        line = "> Received incoming connection from %s:%d" % (addr[0], addr[1])
+        print(line)
+
+        # Start a thread to talk to the remote host
+        proxy_thread = threading.Thread(target=proxy_handler, args=(client_socket, remote_host, remote_port, receive_first))
+        proxy_thread.start()
+
+def main():
+    if len(sys.argv[1:]) != 5:
+        print("Usage: ./proxy.py [localhost] [localport]", end='')
+        print("[remotehost] [remoteport] [receive_first]")
+        print("Example: ./proxy.py 127.0.0.1 9000 10.12.10.100 9000 True")
+        sys.exit(0)
+    
+    local_host = sys.argv[1]
+    local_port = int(sys.argv[2])
+    remote_host = sys.argv[3]
+    remote_port = int(sys.argv[4])
+
+    receive_first = sys.argv[5]
+
+    if "True" in receive_first:
+        receive_first = True
+
+    else:
+        receive_first = False
+
+    server_loop(local_host, local_port, remote_host, remote_port, receive_first)
+
+if __name__ == '__main__':
+    main()
